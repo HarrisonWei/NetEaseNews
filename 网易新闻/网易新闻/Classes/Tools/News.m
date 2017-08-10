5781a3b468eee8d6d8ff1d8133e3d3212c2573da
HarrisonWei <1512909687@qq.com>
2017-08-10 18:32:09 +0800

    动态获取类的属性和关联对象

--- 网易新闻/网易新闻/Classes/Tools/News.m
+++ 网易新闻/网易新闻/Classes/Tools/News.m
@@ -8,6 +8,7 @@
 
 #import "News.h"
 #import "NetworkTools.h"
+#import <objc/runtime.h>
 @implementation News
 - (instancetype)initWithDict:(NSDictionary *)dict{
     self = [super init];
@@ -22,8 +23,55 @@
 - (void)setValue:(id)value forUndefinedKey:(NSString *)key{
     
 }
+//动态加载类的属性
+
+const char *KPopertisKey = "propertisKey";
++ (NSArray *)loadPropertis{
+    //利用关联对象给'类'添加属性,OC中的类本省就是一个特殊对象
+    /*
+     获取关联对象
+     1.对象,属性关联到的对象
+     2.key,属性的key
+     */
+    NSArray *plist = objc_getAssociatedObject(self, KPopertisKey);
+    if (plist != nil) {
+        return plist;
+    }
+    //1.第一个参数:类
+    //2.第二个参数:属性计数指针
+    unsigned int count = 0;
+    objc_property_t *list = class_copyPropertyList([self class], &count);
+    
+    //定义一个可变数组
+    NSMutableArray *nmArray = [NSMutableArray arrayWithCapacity:count];
+    //遍历数组
+    for (unsigned int i = 0; i<count; i++) {
+        objc_property_t pty = list[i];
+        //属性名称
+        const char *cname = property_getName(pty);
+        [nmArray addObject:[NSString stringWithUTF8String:cname]];
+        
+    }
+    NSLog(@"属性数量 %u",count);
+    //释放对象
+    free(list);
+    
+    //设置关联对象属性,在运行时中关联对象使用率最高,主要就是set和get两个方法
+    /*
+     1.属性关联的对象
+     2.key
+     3.值
+     4.引用关系
+     */
+    objc_setAssociatedObject(self, KPopertisKey, nmArray, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    
+    
+    return objc_getAssociatedObject(self, KPopertisKey);
+}
 - (NSString *)description{
-    NSArray *propertis = @[@"title",@"digest",@"replyCount",@"imgsrc"];
+    
+    NSArray *propertis = [self.class loadPropertis];
+    
     NSDictionary *dict = [self dictionaryWithValuesForKeys:propertis];
     return [NSString stringWithFormat:@"<%@: %p> %@",self.class,self,dict];
 }
@@ -32,7 +80,7 @@
         NSLog(@"%@",responseObject.keyEnumerator.nextObject);
         //根据key拿到第一个数组
         NSArray *array = responseObject[responseObject.keyEnumerator.nextObject];
-        NSLog(@"%@",array);
+//        NSLog(@"%@",array);
         //字典转模型
         //Capacity:容量  假设是10 这种创建数组的方式就会一次性分配10 存储空间
         //当添加第11个元素的时候,又会开辟10个存储空间
@@ -48,3 +96,15 @@
     }];
 }
 @end
+
+
+
+
+
+
+
+
+
+
+
+
