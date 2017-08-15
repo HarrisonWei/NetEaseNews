f2c6cee9830d88d1f4abe3f6a8890952249bd37b
HarrisonWei <1512909687@qq.com>
2017-08-16 00:12:18 +0800

    更改约束问题

---
+++ 网易新闻/网易新闻/Classes/Home/Channel/Channel.h
@@ -0,0 +1,13 @@
+//
+//  Channel.h
+//  网易新闻
+//
+//  Created by 曹魏 on 2017/8/15.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Channel : NSObject
+
+@end
