b4ca766fb5c5b5aaa7d572ae70910f5e1db1f496
HarrisonWei <1512909687@qq.com>
2017-08-17 23:21:44 +0800

    修复文件的感叹号和问号

---
+++ 网易新闻/网易新闻/Classes/Tools/NSObject+Extension.m
@@ -0,0 +1,72 @@
+//
+//  NSObject+Extension.m
+//  网易新闻
+//
+//  Created by 曹魏 on 2017/8/17.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import "NSObject+Extension.h"
+#import <objc/runtime.h>
+#import "News.h"
+@implementation NSObject (Extension)
+#pragma mark
+#pragma mark -字典转模型
++ (instancetype)objectWithDict:(NSDictionary *)dict{
+    News *news = [[News alloc]init];
+    [news setValuesForKeysWithDictionary:dict];
+    
+    return news;
+}
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key{
+    
+}
+#pragma mark
+#pragma mark -运行时方法
+//动态加载类的属性
+
+const char *KPopertisKey = "propertisKey";
++ (NSArray *)loadPropertis{
+    //利用关联对象给'类'添加属性,OC中的类本省就是一个特殊对象
+    /*
+     获取关联对象
+     1.对象,属性关联到的对象
+     2.key,属性的key
+     */
+    NSArray *plist = objc_getAssociatedObject(self, KPopertisKey);
+    if (plist != nil) {
+        return plist;
+    }
+    //1.第一个参数:类
+    //2.第二个参数:属性计数指针
+    unsigned int count = 0;
+    objc_property_t *list = class_copyPropertyList([self class], &count);
+    
+    //定义一个可变数组
+    NSMutableArray *nmArray = [NSMutableArray arrayWithCapacity:count];
+    //遍历数组
+    for (unsigned int i = 0; i<count; i++) {
+        objc_property_t pty = list[i];
+        //属性名称
+        const char *cname = property_getName(pty);
+        [nmArray addObject:[NSString stringWithUTF8String:cname]];
+        
+    }
+    NSLog(@"属性数量 %u",count);
+    //释放对象
+    free(list);
+    
+    //设置关联对象属性,在运行时中关联对象使用率最高,主要就是set和get两个方法
+    /*
+     1.属性关联的对象
+     2.key
+     3.值
+     4.引用关系
+     */
+    objc_setAssociatedObject(self, KPopertisKey, nmArray, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    
+    
+    return objc_getAssociatedObject(self, KPopertisKey);
+}
+
+@end
--- 网易新闻/网易新闻/Classes/Tools/News.m
+++ 网易新闻/网易新闻/Classes/Tools/News.m
@@ -8,66 +8,8 @@
 
 #import "News.h"
 #import "NetworkTools.h"
-#import <objc/runtime.h>
+#import "NSObject+Extension.h"
 @implementation News
-- (instancetype)initWithDict:(NSDictionary *)dict{
-    self = [super init];
-    if (self) {
-        [self setValuesForKeysWithDictionary:dict];
-    }
-    return self;
-}
-+ (instancetype)newsWithDict:(NSDictionary *)dict{
-    return [[self alloc]initWithDict:dict];
-}
-- (void)setValue:(id)value forUndefinedKey:(NSString *)key{
-    
-}
-//动态加载类的属性
-
-const char *KPopertisKey = "propertisKey";
-+ (NSArray *)loadPropertis{
-    //利用关联对象给'类'添加属性,OC中的类本省就是一个特殊对象
-    /*
-     获取关联对象
-     1.对象,属性关联到的对象
-     2.key,属性的key
-     */
-    NSArray *plist = objc_getAssociatedObject(self, KPopertisKey);
-    if (plist != nil) {
-        return plist;
-    }
-    //1.第一个参数:类
-    //2.第二个参数:属性计数指针
-    unsigned int count = 0;
-    objc_property_t *list = class_copyPropertyList([self class], &count);
-    
-    //定义一个可变数组
-    NSMutableArray *nmArray = [NSMutableArray arrayWithCapacity:count];
-    //遍历数组
-    for (unsigned int i = 0; i<count; i++) {
-        objc_property_t pty = list[i];
-        //属性名称
-        const char *cname = property_getName(pty);
-        [nmArray addObject:[NSString stringWithUTF8String:cname]];
-        
-    }
-    NSLog(@"属性数量 %u",count);
-    //释放对象
-    free(list);
-    
-    //设置关联对象属性,在运行时中关联对象使用率最高,主要就是set和get两个方法
-    /*
-     1.属性关联的对象
-     2.key
-     3.值
-     4.引用关系
-     */
-    objc_setAssociatedObject(self, KPopertisKey, nmArray, OBJC_ASSOCIATION_COPY_NONATOMIC);
-    
-    
-    return objc_getAssociatedObject(self, KPopertisKey);
-}
 - (NSString *)description{
     
     NSArray *propertis = [self.class loadPropertis];
@@ -80,8 +22,7 @@ const char *KPopertisKey = "propertisKey";
     //断言
     NSAssert(finished != nil, @"传入必须完成回调");
     
-    
-    [[NetworkTools sharedNetworkTools]GET:urlString parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *responseObject) {
+    [[NetworkTools sharedNetworkTools]GET:urlString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *responseObject) {
         NSLog(@"%@",responseObject.keyEnumerator.nextObject);
         //根据key拿到第一个数组
         NSArray *array = responseObject[responseObject.keyEnumerator.nextObject];
@@ -93,14 +34,15 @@ const char *KPopertisKey = "propertisKey";
         
         NSMutableArray *arrayM = [NSMutableArray arrayWithCapacity:array.count];
         for (NSDictionary *obj in array) {
-            [arrayM addObject:[self newsWithDict:obj]];
+            [arrayM addObject:[self objectWithDict:obj]];
         }
         //回调,AFN的回调是在主线程
         finished(arrayM.copy);
-        
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         NSLog(@"%@",error);
     }];
+    
+    
 }
 @end
 
